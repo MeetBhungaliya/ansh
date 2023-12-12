2b31393ee25e2a1fb8905af0337873b4fa8af823
YashB1702 <yash.revolo@gmail.com>
2023-12-12 21:04:48 +0530

    instagram link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      Ansh
-    </div>
+    <section className='h-full min-h-screen bg-black text-white flex justify-center items-center'>
+      <a href="https://www.instagram.com/mr_kajavadra/?igshid=NGVhN2U2NjQ0Yg%3D%3D&utm_source=qr" className='text-4xl hover:underline transition-all' target='_ansh'>
+        My Instagram 😎
+        </a>
+    </section>
   )
 }
 
